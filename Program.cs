673a0a9daf0f04c3a43f802a76a926608bee4194
mmorillo12 <78889380+mmorillo12@users.noreply.github.com>
2021-02-28 16:35:19 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace orden_contrario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Un programa que pida al usuario 5 números reales (pista: necesitarás un array de
+           "float") y luego los muestre en el orden contrario al que se introdujeron.*/
+           float[] numeros = new float [5];
+           Console.WriteLine("Ingrese sus cinco numeros: ");
+           
+           //Aqui conseguimos los valores que desea el usuario
+           for(int i = 0; i <= 4; i++){
+               numeros[i] = float.Parse(Console.ReadLine());
+           }
+           //Limpio pantalla para que se vea mas ordenado el terminal
+           Console.Clear();
+           
+           //Este es un metodo que pone al contrario las valores de tu lista segun los ingresaste
+           Array.Reverse(numeros);
+           Console.WriteLine("Sus valores ingresados de forma contraria serian: ");
+           
+           //Aqui evaluamos cada elemento de la lista que ya estan al contrario y los imprimimos
+           foreach(var i in numeros){
+               Console.WriteLine(i);
+           }
+        }
+    }
+}
