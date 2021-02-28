69045d7d5810acbb53379055f1896a85163514db
mmorillo12 <78889380+mmorillo12@users.noreply.github.com>
2021-02-28 16:59:41 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace separar_letras
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      /*Un programa que te pida tu nombre y lo muestre en pantalla separando cada letra de la
+      siguiente con un espacio. Por ejemplo, si tu nombre es "Juan", debería aparecer en pantalla "J u a n".*/
+      string[] nombre = new string[1];
+      for(int i = 0; i < nombre.Length; i++){
+         nombre[i] = Console.ReadLine();
+      }
+      char[] lsn = nombre[0].ToCharArray();
+      for(int i = 0; i < lsn.Length; i++){
+        Console.WriteLine(lsn[i]);
+      }
+    }
+  }
+}
