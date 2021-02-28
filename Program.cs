d4c23e58606a19d99e134949324515f1ceed5cd0
mmorillo12 <78889380+mmorillo12@users.noreply.github.com>
2021-02-28 16:40:07 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace mayor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Un programa que pida al usuario 10 números y luego calcule y muestre cuál es
+            el mayor de todos ellos.*/
+            int[] numeros = new int[10];
+            int mayor = numeros[0];
+
+            Console.WriteLine("Profavor Ingrese 10 valores para determinar el mayor");
+            for(int i = 0; i<= 9; i++){
+                numeros[i] = int.Parse(Console.ReadLine());
+            }
+            Console.Clear();
+            for(int i = 0; i<=9; i++){
+                if(numeros[i] > mayor)
+                    mayor = numeros[i];
+                
+            }
+            Console.WriteLine("El mayor numero es: " + mayor);
+
+        }
+    }
+}
